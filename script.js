dd56627182072deee67302cb2a1176317b027409
Federico Pizzeghello <pizzeghello.work@gmail.com>
2022-01-26 22:18:32 +0100

    possibile soluzione

--- script.js
+++ script.js
@@ -26,27 +26,11 @@ const text = [
 let up = document.getElementById('arrowUp')
 let down = document.getElementById('arrowDown')
 
-let x =  0;
-
-up.addEventListener('click',
-    function() {
-        x = x - 1;
-        opa[x].classList.add('active2');
-        bk[x].classList.add('active')
-    }
-);
-
-down.addEventListener('click',
-    function () {
-        x = x + 1;
-        opa[x].classList.add('active2');
-        bk[x].classList.add('active')
-    }
-)
-
+let x = 0;
 
 // CAMBIANO IMMAGINI BLOCCO 1
 let slider = ''
+let rightImg = ''
 for (let i = 0; i < items.length; i++) {
     slider +=   `<div class="bk"> 
                     <img src="${items[i]}" alt="">
@@ -54,6 +38,10 @@ for (let i = 0; i < items.length; i++) {
                         <h1>${title[i]}</h1>
                         <h4>${text[i]}</h4>
                     </div>
+                </div>`;
+
+    rightImg += `<div class="picture"> 
+                    <img src="${items[i]}" alt="">
                 </div>`;
 }
 
@@ -66,21 +54,38 @@ bk[x].classList.add('active')
 
 // -----------------------------------------------------------
 
-// CAMBIANO IMMAGINI BLOCCO 2
-let rightImg = ''
-for (let i = 0; i < items.length; i++) {
-    rightImg += `<div class="picture"> 
-                    <img src="${items[i]}" alt="">
-                </div>`;
-}
 
 // INSERISCO IMMAGINI NELL'HTML
 let block2 = document.querySelector('.block2');
 block2.innerHTML = rightImg
 // AGGIUNGO LA CLASSE ACTIVE2
 let opa = document.getElementsByClassName('picture');
-opa[x].classList.add('active2');
 
 
 
+var slideIndex = 1;
+showDivs(slideIndex);
 
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("bk");
+  var border = document.getElementsByClassName("picture");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  for (i = 0; i < border.length; i++) {
+    border[i].className = border[i].className.replace("active2");
+  }
+  x[slideIndex-1].style.display = "block";  
+  border[slideIndex-1].className += " active2";
+}
